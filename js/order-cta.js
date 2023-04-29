11f8d286db81103eec506f2a9085316e1391bc78
misonggg <codingprac96@gmail.com>
2023-04-29 12:44:13 +0900

    🚧 Implement toggling bookmark button

--- js/order-cta.js
+++ js/order-cta.js
@@ -17,3 +17,32 @@ function CloseOrderModal() {
 }
 
 orderModalOverlay.addEventListener('click', CloseOrderModal)
+
+
+// 북마크 부분 -----------------------------------------------------------------
+orderCtaBookmarkButton.addEventListener('click', toggleOrderCtaBookmark)
+
+function toggleOrderCtaBookmark() {
+  const [icon, countSpan] = this.children
+  const count = Number(countSpan.innerHTML.replaceAll(',', ''))
+
+  let newCount = count
+
+  // 활성화가 된 상태이니 -> 비활성화 (ic-bookmark)
+  if (this.classList.contains('is-active')) {
+    icon.classList.add('ic-bookmark')
+    icon.classList.remove('ic-bookmark-filled')
+    newCount = newCount - 1
+  } else {
+  // 비활성화가 된 상태이니 -> 활성화 (ic-bookmark-filled)
+  icon.classList.add('ic-bookmark-filled')
+  icon.classList.remove('ic-bookmark')
+  newCount = newCount + 1
+  }
+
+  countSpan.innerHTML = newCount.toLocaleString()
+
+  this.classList.toggle('is-active')
+
+}
+
