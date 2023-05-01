68c0412a8101b1ae39fce48e4cfda8ed90dc78a1
misonggg <codingprac96@gmail.com>
2023-05-01 16:58:54 +0900

    🚧 Implement toggling my-menu

---
+++ js/my-menu.js
@@ -0,0 +1,23 @@
+// my-menu , my-menu-content, my-menu-button
+
+const myMenu = document.querySelector('.my-menu')
+const myMenuButton = document.querySelector('.my-menu-button')
+const myMenuContent = document.querySelector('.my-menu-content')
+
+console.log(myMenu, myMenuButton, myMenuContent)
+
+myMenuButton.addEventListener('click', toggleMyMenu)
+
+function toggleMyMenu() {
+  if(!myMenu.classList.contains('is-active')) {
+    myMenu.classList.toggle('is-active')
+    window.addEventListener('click', closeMyMenuOnClickingOutside)
+  }
+}
+
+function closeMyMenuOnClickingOutside (e) {
+  if(!myMenu.contains(e.target)) {
+    myMenu.classList.remove('is-active')
+    window.removeEventListener('click', closeMyMenuOnClickingOutside)
+  }
+}
